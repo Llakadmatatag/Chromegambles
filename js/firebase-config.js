44ab13d896f3d45bdea281f511437cf14f51eb9b
Sentroek008 <agun9wib93@gmail.com>
2025-05-09 02:52:21 +0700

    Update Firebase configuration with actual values

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
   // Masukkan konfigurasi Firebase Anda di sini
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyAVUqUZvYLOfglGRdgKrIsG6k6KDAxvFu4",
+  authDomain: "xfun-lb-web-chrome.firebaseapp.com",
+  projectId: "xfun-lb-web-chrome",
+  storageBucket: "xfun-lb-web-chrome.firebasestorage.app",
+  messagingSenderId: "749066968777",
+  appId: "1:749066968777:web:1482e4011829dbce35f718",
+  measurementId: "G-BFDDTMSPX3"
 };
 
 // Initialize Firebase
